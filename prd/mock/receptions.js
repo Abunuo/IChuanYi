6204737c09df782ea2557fe115fbed1b32eb2547
Abunuo <liangmingqing315@163.com>
2017-05-26 11:12:55 +0800

    添加用户评价数据

---
+++ prd/mock/receptions.js
@@ -0,0 +1,43 @@
+module.exports=[
+  {
+    userHead: '/images/sh6.jpg',
+    name: 'wuli小妹',
+    content: '宝贝收到了，包装严实，高端；衣服手感也可以哈哈，好评美美哒。',
+    productInfo: {
+      color: '冰激凌印花',
+      size: 'S'
+    }
+  },{
+    userHead: '/images/sh7.jpg',
+    name: 'Monologue',
+    content: '物流给力，客服很细心的介绍；衣服很舒服，价钱很合理，好评。',
+    productInfo: {
+      color: '巧克力雪花',
+      size: 'M'
+    }
+  },{
+    userHead: '/images/sh8.jpg',
+    name: 'Abunuo',
+    content: '宝贝没有色差，跟图片一样，很喜欢~',
+    productInfo: {
+      color: ' 卡其色星纹',
+      size: 'SM'
+    }
+  },{
+    userHead: '/images/sh9.jpg',
+    name: 'wuli小姐姐',
+    content: '衣服很舒服，质量很好，物美价廉，下次购物首选。',
+    productInfo: {
+      color: '红色尊贵',
+      size: 'L'
+    }
+  },{
+    userHead: '/images/sh3.jpg',
+    name: '放羊的星星',
+    content: '客服态度很好，很愉快的一次购物 O.O',
+    productInfo: {
+      color: '紫色星空',
+      size: 'SM'
+    }
+  }
+]
